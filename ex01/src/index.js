1c88e899134c328b1a65079ae3cd2f22d4ec2249
badnc <79924957+badnc@users.noreply.github.com>
2021-09-15 17:41:49 +0200

    exercise day03

---
+++ ex01/src/index.js
@@ -0,0 +1,7 @@
+function main() {
+    // Only change code below this line
+    console.log("Hello World!");
+    // Only change code abowe this line 
+}
+main();
+module.exports = main;
---
+++ ex02/src/index.js
@@ -0,0 +1,8 @@
+function main() {
+    // This is single line JavaScript comment
+    /* This is single line JavaScript comment
+       This is single line JavaScript comment
+       This is single line JavaScript comment */
+}
+console.log(main());
+module.exports = main;
